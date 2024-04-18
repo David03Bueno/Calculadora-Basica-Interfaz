4c7fd48afb0b5f16a6c495fbe65625469d48714e
David_Bueno <david03bueno@gmail.com>
2024-04-18 00:49:47 -0400

    Calculadora basica en java SWING

---
+++ Calculadoraprincipal/src/main/java/tarea/calculadoraprincipal/Calculadora.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tarea.calculadoraprincipal;
+
+/**
+ *
+ * @author david
+ * @matricula 2023-0243
+ */
+public class Calculadora {
+
+    public double sumar(double valor1, double valor2) {
+        return valor1 + valor2;
+    }
+
+    public double restar(double valor1, double valor2) {
+        return valor1 - valor2;
+    }
+
+    public double multiplicar(double valor1, double valor2) {
+        return valor1 * valor2;
+    }
+
+    public double dividir(double valor1, double valor2) {
+        return valor1 / valor2;
+    }
+}
---
+++ Calculadoraprincipal/src/main/java/tarea/calculadoraprincipal/Calculadoraprincipal.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package tarea.calculadoraprincipal;
+
+/**
+ *
+ * @author david
+ */
+public class Calculadoraprincipal {
+
+    public static void main(String[] args) {
+
+
+    }
+}
